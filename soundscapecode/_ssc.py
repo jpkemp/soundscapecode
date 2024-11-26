fe6435e17ee057c566d772c0c97267fcac289982
James Kemp <z3448326@ad.unsw.edu.au>
2024-11-26 17:31:12 +0800

    updates to ssc, not working

--- soundscapecode/_ssc.py
+++ soundscapecode/_ssc.py
@@ -4,12 +4,10 @@ from soundscapecode import _soundscape_code as ssc
 class SoundscapeCode:
     '''Wrapper for segmenting and calculating soundscape code metrics for a sound.
 
-    Parameters
+    Attributes
     ----------
     sound: np.ndarray
-        the sound to analyse. The sound will be segmented into one-minute blocks.
-    fs: int
-        sampling frequency for the sound
+        the sound recording to calculate the metrics for
 
     Examples
     -----
@@ -26,16 +24,27 @@ class SoundscapeCode:
     -7.38333472594301e-06
     '''
 
-    def __init__(self, sound:np.ndarray, fs:int):
+    def __init__(self, sound:np.ndarray, fs:int, freq_range=None):
+        '''
+        Parameters
+        ----------
+        sound: np.ndarray
+            the sound to analyse. The sound will be segmented into one-minute blocks.
+        fs: int
+            sampling frequency for the sound
+        '''
         one_min_interval = fs * 60
-        sound:np.ndarray = sound
+        self.sound:np.ndarray = sound
         self.fs:int = fs
         self.sounds:list[np.ndarray] = []
         self.kurtosis:list[np.float] = []
         self.periodicity:list[np.float] = []
         self.Lppk:list[np.float] = []
         self.Lprms:list[np.float] = []
+        self.dissimilarities:list[np.float] = []
+        self.spectral_dissimilarities:list[np.float] = []
         self.temporal_dissimilarities:list[np.float] = []
+        self.freq_range = freq_range
         for i in range(0, len(sound), one_min_interval):
             self.sounds.append(sound[i:i+one_min_interval])
 
@@ -53,6 +62,10 @@ class SoundscapeCode:
             return self.Lppk
         if lower in ["dt", "temporal", "temporal_dissimilarity", "dissimilarity_temporal"]:
             return self.temporal_dissimilarities
+        if lower in ["df", "spectral", "spectral_dissimilarity", "dissimilarity_spectral"]:
+            return self.spectral_dissimilarities
+        if lower in ["d", "dissimilarity", "dissimilarity_index"]:
+            return self.dissimilarities
 
         return NotImplemented
 
@@ -75,3 +88,15 @@ class SoundscapeCode:
 
             dis = ssc.temporal_dissimilarity(a, b)
             self.temporal_dissimilarities.append(dis)
+
+        f, t, pxx = ssc.power_spectral_density(self.sound, self.fs)
+        meanfreqs = ssc.meanfreq(pxx, f, self.freq_range)
+        one_min = 120
+        remainder = t.shape[0] % one_min
+        for segment_start in range(0, t.shape[0] - remainder - one_min, one_min):
+            a = meanfreqs[segment_start:segment_start+one_min]
+            b = meanfreqs[segment_start+one_min:segment_start+2*one_min]
+            spectral_dissimilarity = ssc.spectral_dissimilarity(a, b)
+            self.spectral_dissimilarities.append(spectral_dissimilarity)
+
+        self.dissimilarities = list(np.array(self.temporal_dissimilarities) * np.array(self.spectral_dissimilarities))
