27bc0cbe07732392ed8c63a501d999e5023ac73d
jpkemp <mailjpk@gmail.com>
2024-12-04 16:29:37 +0800

    filter progress. need to think about normalised vs standard

--- soundscapecode/filters.py
+++ soundscapecode/filters.py
@@ -1,3 +1,4 @@
+from operator import add, sub
 import numpy as np
 from math import log10, sqrt, floor
 from scipy.signal import lfilter, filtfilt, butter, kaiserord, firwin, freqz
@@ -89,6 +90,37 @@ def convert_mag_units(val, source, target, band):
 
     raise UnknownSourceError(source)
 
+def check_kaiser_specs(b, stopbands, passbands, fs):
+    N = 2**12
+    n_points = len(stopbands)
+    for idx in range(1, n_points):
+        Fstart = stopbands[idx - 1] if idx else 0
+        Fend = stopbands(idx) if idx else fs
+        #Get fft at desired bands, we always get normalized frequency values in
+        #hspecs so set Fs parameter to 2
+        linN = np.linspace(Fstart, Fend, N)
+        h = abs(freqz(b, worN=N, fs=fs))
+        
+        # Measure attenuation defined as the distance between the nominal
+        # gain(0 dB in our case) and the maximum rippple in the stopband.
+        ngain = 1
+        measAstop = db(ngain)-db(max(h))
+        if (measAstop <= Astop(idx))
+            return #return with status = false
+                #Measure ripple at the passbands
+    N = 2**10
+    for idx = 1:size(passbands,1)
+        Fstart = passbands(idx,1)
+        Fend = passbands(idx,2)
+        #Get fft at desired bands, we always get normalized frequency values in
+        #hspecs so set Fs parameter to 2    
+        h = abs(freqz(b, worN=N, fs))
+        # The ripple is defined as the amplitude (dB) variation between the two
+        # specified frequency points.
+        measApass = db(max(h))-db(min(h))
+        if (measApass >= Apass(idx))
+            return #return with status = false
+
 def highpass(data, passband:tuple, fs:int, steepness=0.85, stopband_atten=60):
     if steepness < 0.5 or steepness > 1:
         raise ValueError("Steepness must be between 0.5 and 1")
@@ -104,9 +136,39 @@ def highpass(data, passband:tuple, fs:int, steepness=0.85, stopband_atten=60):
     trans_width = passband - Wstop
 
     numtaps, beta = kaiserord(stopband_atten, trans_width/(0.5*fs))
-    mid_freq = passband - (trans_width / 2)
-    taps = firwin(numtaps, mid_freq, width=trans_width, scale=True, fs=fs, pass_zero='highpass')
+    cutoff = passband - (trans_width / 2)
+    taps = firwin(numtaps, cutoff, width=trans_width, scale=True, fs=fs, pass_zero='highpass')
     # taps = firwin(numtaps, mid_freq, window=('kaiser', beta), scale=True, fs=fs, pass_zero='highpass')
+    # taps = firwin(numtaps, mid_freq / (fs/2), window=('kaiser', beta), scale=True, fs=fs, pass_zero='highpass')
+    delay = floor(numtaps / 2)
+    temp_data = np.concatenate([data, np.zeros(delay)])
+    fltrd = lfilter(taps, 1, temp_data)[delay:]
+
+    return fltrd
+
+def bandpass(data, low, high, fs, steepness=0.85, stopband_atten=60):
+    if steepness < 0.5 or steepness > 1:
+        raise ValueError("Steepness must be between 0.5 and 1")
+
+    nyquist = fs / 2
+    TwPercentage = -0.98*steepness + 0.99
+    # ripple = 0.1
+    trans_widths = []
+    cutoffs = []
+    for passband, op in [(low, sub), (high, add)]:
+        WpassNormalized = passband/(nyquist)
+        Tw = TwPercentage * WpassNormalized if op is sub else TwPercentage * (1 - WpassNormalized)
+        WstopNormalized = op(WpassNormalized, Tw)
+        Wstop = WstopNormalized * (nyquist)
+        trans_width = abs(passband - Wstop)
+        trans_widths.append(trans_width)
+        mid_freq = op(passband, trans_width / 2) # unclear whether this should be min trans_width or relative
+        cutoffs.append(mid_freq)
+
+    trans_width = min(trans_widths)
+    numtaps, beta = kaiserord(stopband_atten, trans_width/(nyquist))
+    taps = firwin(numtaps, cutoffs, width=trans_width, scale=True, fs=fs, pass_zero='bandpass')
+    # taps = firwin(numtaps, mid_freqs, window=('kaiser', beta), scale=True, fs=fs, pass_zero='bandpass')
     delay = floor(numtaps / 2)
     temp_data = np.concatenate([data, np.zeros(delay)])
     fltrd = lfilter(taps, 1, temp_data)[delay:]
--- tests/test_filters.py
+++ tests/test_filters.py
@@ -1,13 +1,23 @@
 import unittest
 import numpy as np
 import pandas as pd
-from soundscapecode.filters import highpass
+from soundscapecode.filters import highpass, bandpass
 from soundscapecode.soundtrap import open_wav
 
 class TestFilters(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.fs, cls.sig = open_wav('data/7255.221112060000.wav', trim_start=3, soundtrap=7255)
+        return super().setUpClass()
+
     def test_highpass(self):
-        fs, sig = open_wav('data/7255.221112060000.wav', trim_start=3, soundtrap=7255)
-        fltrd = highpass(sig, 200, fs)
+        fltrd = highpass(self.sig, 200, self.fs)
         expected = np.squeeze(pd.read_csv("data/7255_broad.csv", header=None).values)
         self.assertTrue(np.allclose(fltrd, expected, 10e2))
-        
+
+    def test_bandpass(self):
+        for fl, freqs in [("data/7255_fish.csv", (200, 800)), 
+                          ("data/7255_invertebrate.csv", (2000, 5000))]:
+            expected = np.squeeze(pd.read_csv(fl, header=None).values)
+            fltrd = bandpass(self.sig, *freqs, self.fs)
+            self.assertTrue(np.allclose(fltrd, expected, 10e2))
